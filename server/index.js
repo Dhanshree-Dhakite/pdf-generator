4fe806ddf2be273020e1112e9117dc858d161029
Dhanshree Dhakite <dhanshreedhakite12345@gmail.com>
2024-08-12 10:51:56 +0530

    pdf-generator

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const pdfTemplate = require("./documents");
+const htmlPdf = require("html-pdf");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/create-pdf", (req, res) => {
+  htmlPdf.create(pdfTemplate(req.body), {}).toFile("result.pdf", (err) => {
+    if (err) {
+      res.send(Promise.reject());
+    }
+    res.send(Promise.resolve());
+  });
+});
+
+app.get("/fetch-pdf", (req, res) => {
+  res.sendFile(`${__dirname}/result.pdf`);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
