2c5f74a5c480c6c952021350064469e9c5dcc25e
Dhanshree Dhakite <dhanshreedhakite12345@gmail.com>
2024-08-13 22:09:39 +0530

    pdf-generator-new

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const htmlPdf = require("html-pdf");
 const app = express();
 const port = process.env.PORT || 5000;
 
-//Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
